14972ecd977a9922752c6b229dfa85638af0fcff
Brendan Burns <boardslide03@Brendans-MacBook-Pro-2.local>
2016-11-14 23:03:24 -0800

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 $(document).ready(function() {
 
-    $(document).on("click", ".tic-cell", function() {
+// Step 1: Bind a click event to the .box class
+// Step 2: Check if square is empty
+// Step 3: Change turn
+// Step 4: Update the HTML with the appropriate letter
+
+// Start off app with currentTurn = X
+    var currentTurn = "X";
+
+// Bind a click event to all squares
+    $(".box").on("click", function() {
+        // console.log($(this).html(), "LETTER SELECTED");
+
+        // Check to see if inner HTML is empty, if so continue logic
+        if($(this).html() === "") {
+            // Set the HTML of the empty element to whatever the turn is
+            $(this).html(currentTurn);
+
+            // Flip the turn to the next player
+            if (currentTurn === "X") {
+                currentTurn = "O";
+            } else {
+                currentTurn = "X";
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+    $(document).on("click", "box", function() {
     alert($(this).html());
 });
 
@@ -11,3 +46,21 @@ $(document).ready(function() {
     });
 
 });
+
+
+// var $boxes = $(".boxes");
+// var flag = 0;
+//
+// $boxes.on("click", function() {
+//     if (!$(this).hasClass("done")) {
+//         if (flag === 0) {
+//             $(this).html("X");
+//             flag = 1;
+//             $(this).addClass("done");
+//         } else {
+//             $(this).html("0");
+//             flag = 0;
+//             $(this).addClass("done");
+//         }
+//     }
+// }
